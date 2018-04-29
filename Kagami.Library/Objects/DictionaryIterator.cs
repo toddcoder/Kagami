5ca49d2d50050b680c413d2c0ee5d2f87ddd8570
Todd bennett <toddcoder@hotmail.com>
2018-04-28 19:59:24 -0500

    New dictionary iterators (lazy & not lazy)

--- Kagami.Library/Objects/DictionaryIterator.cs
+++ Kagami.Library/Objects/DictionaryIterator.cs
@@ -1,4 +1,6 @@
-﻿using Standard.Types.Maybe;
+﻿using System.Collections.Generic;
+using Kagami.Library.Runtime;
+using Standard.Types.Maybe;
 using static Standard.Types.Maybe.MaybeFunctions;
 
 namespace Kagami.Library.Objects
@@ -14,8 +16,30 @@ namespace Kagami.Library.Objects
          keys = dictionary.KeyArray;
       }
 
-      public override IMaybe<IObject> Next() => when(index++ < keys.Length, () => dictionary[keys[index]]);
+      public override IMaybe<IObject> Next() => when(index < keys.Length, () =>
+      {
+         var key = keys[index++];
+         return Tuple.NewTuple(key, dictionary[key]).Some();
+      });
 
-      public override IMaybe<IObject> Peek() => when(index < keys.Length, () => dictionary[keys[index]]);
+      public override IMaybe<IObject> Peek() => when(index < keys.Length, () =>
+      {
+         var key = keys[index];
+         return Tuple.NewTuple(key, dictionary[key]).Some();
+      });
+
+      public override IEnumerable<IObject> List()
+      {
+         var item = none<IObject>();
+         index = 0;
+         do
+         {
+            item = Next();
+            if (item.If(out var obj))
+               yield return obj;
+            if (index % 1000 == 0 && Machine.Current.Context.Cancelled())
+               yield break;
+         } while (item.IsSome);
+      }
    }
 }
