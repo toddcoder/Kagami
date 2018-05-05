8ff14da2c1167209a8c988a8b6e1a410f157b95f
Todd bennett <toddcoder@hotmail.com>
2018-05-05 18:01:46 -0500

    $c format defaults to 2 decimal points

--- Kagami.Library/Parsers/Expressions/FormatParser.cs
+++ Kagami.Library/Parsers/Expressions/FormatParser.cs
@@ -14,8 +14,14 @@ namespace Kagami.Library.Parsers.Expressions
       public override IMatched<Unit> Parse(ParseState state, Token[] tokens, ExpressionBuilder builder)
       {
          var input = tokens[2].Text.Skip(1);
-         if (!input.Contains(".") && input.IsMatch("^ ['cefgnp']"))
-            input += ".6";
+         if (!input.Contains("."))
+         {
+            if (input.IsMatch("^ ['efgnp']"))
+               input += ".6";
+            else if (input == "c")
+               input += ".2";
+         }
+
          state.Colorize(tokens, Color.Whitespace, Color.Format);
 
          builder.Add(new StringSymbol(input));
