86e6d021771091461f2331b57e9b860b352f7edd
Todd bennett <toddcoder@hotmail.com>
2018-08-29 20:58:36 -0500

    Fixed dynamic response

--- Kagami.Library/Classes/SelectorSet.cs
+++ Kagami.Library/Classes/SelectorSet.cs
@@ -1,37 +1,47 @@
-﻿using Kagami.Library.Objects;
-using Kagami.Library.Runtime;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kagami.Library.Objects;
 using Standard.Types.Collections;
 
 namespace Kagami.Library.Classes
 {
    public class SelectorSet : Set<string>
    {
-      public override void Add(string item)
+	   AutoHash<string, List<string>> buckets;
+
+	   public SelectorSet() => buckets = new AutoHash<string, List<string>>(key => new List<string>(), true);
+
+	   public void Add(Selector selector)
       {
-         if (!item.EndsWith(")"))
-            item = $"{item}()";
-         base.Add(item);
+         base.Add(selector);
+	      buckets[selector.LabelsOnly().Image].Add(selector);
       }
 
-      public bool Contains(Selector selector)
+	   public void AddRange(IEnumerable<Selector> selectors)
+	   {
+		   foreach (var selector in selectors)
+			   Add(selector);
+	   }
+
+	   public new void AddRange(IEnumerable<string> sources) => AddRange(sources.Select(s => (Selector)s));
+
+	   public bool Contains(Selector selector)
       {
-         if (base.Contains(selector.Image))
-            return true;
-         else
-         {
-            var count = selector.SelectorItems.Length;
-            var iterator = new BitIterator(count);
-            foreach (var bools in iterator)
-            {
-               var newSelector = selector.Equivalent(bools);
-               if (base.Contains(newSelector))
-                  return true;
-            }
+	      if (base.Contains(selector.Image))
+		      return true;
+	      else
+	      {
+		      var labelsOnlyImage = selector.LabelsOnly().Image;
+				if (buckets.ContainsKey(labelsOnlyImage))
+					foreach (var bucket in buckets[labelsOnlyImage])
+					{
+						Selector matchSelector = bucket;
+						if (selector.IsEquivalentTo(matchSelector))
+							return true;
+					}
 
-            var labelsOnly = selector.LabelsOnly();
-
-            return base.Contains(labelsOnly.Image);
-         }
+		      return base.Contains(labelsOnlyImage);
+	      }
       }
    }
 }
