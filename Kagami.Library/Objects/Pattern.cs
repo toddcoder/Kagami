3a9b560fe66a75d60754eda2683be08d96260e3a
Todd bennett <toddcoder@hotmail.com>
2019-07-05 18:49:38 -0500

    Saved for later

--- Kagami.Library/Objects/Pattern.cs
+++ Kagami.Library/Objects/Pattern.cs
@@ -34,41 +34,44 @@ namespace Kagami.Library.Objects
 
 		public string AsString => name;
 
-		public string Image => $"pattern {name}({lambda.Invokable.Parameters.Select(p => p.Name).Stringify()})";
+		public virtual string Image => $"pattern {name}({lambda.Invokable.Parameters.Select(p => p.Name).Stringify()})";
 
 		public int Hash => Image.GetHashCode();
 
-		public bool IsEqualTo(IObject obj)
+		public virtual bool IsEqualTo(IObject obj)
 		{
-			if (obj is Pattern pattern)
-			{
-				return name == pattern.name && lambda.IsEqualTo(pattern.lambda);
-			}
-			else
-			{
-				return false;
-			}
+			return obj is Pattern pattern && name == pattern.name && lambda.IsEqualTo(pattern.lambda);
 		}
+
+		public IObject Value { get; set; } = Boolean.False;
 
 		protected bool returnValues(IObject result, Hash<string, IObject> bindings)
 		{
 			switch (result)
 			{
 				case Boolean boolean when arguments.Length == 0:
+					Value = boolean;
 					return boolean.Value;
 				case Boolean boolean when arguments.Length == 1:
+					Value = boolean;
 					return match(boolean, arguments[0], bindings);
 				case Some some when arguments.Length == 1:
+					Value = some.Value;
 					return match(some.Value, arguments[0], bindings);
+				case SubPattern subPattern:
+					Value = subPattern;
+					return true;
 				default:
 					if (result is Some tupleSome && tupleSome.Value is Tuple tuple && tuple.Length.Value == arguments.Length)
 					{
+						Value = tuple;
 						return tuple.Value.Zip(arguments, (l, r) => match(l, r, bindings)).All(b => b);
 					}
 
 					break;
 			}
 
+			Value = Boolean.False;
 			return false;
 		}
 
@@ -80,7 +83,7 @@ namespace Kagami.Library.Objects
 			return returnValues(result, bindings);
 		}
 
-		public void RegisterArguments(Arguments arguments)
+		public virtual void RegisterArguments(Arguments arguments)
 		{
 			var fieldValues = arguments.Take(fields.Length).ToArray();
 			var parameterNames = parameters.Select(p => p.Name).ToArray();
@@ -95,6 +98,8 @@ namespace Kagami.Library.Objects
 
 		public bool IsTrue => true;
 
-		public Pattern Copy() => new Pattern(name, lambda, parameters);
+		public virtual Pattern Copy() => new Pattern(name, lambda, parameters);
+
+		public string Name => name;
 	}
 }
--- Kagami.Library/Objects/SubPattern.cs
+++ Kagami.Library/Objects/SubPattern.cs
@@ -1,27 +1,43 @@
 ﻿using Core.Collections;
-using Core.Monads;
 using Kagami.Library.Invokables;
-using static Core.Monads.MonadFunctions;
 
 namespace Kagami.Library.Objects
 {
 	public class SubPattern : Pattern
 	{
 		protected Pattern parentPattern;
-		protected IMaybe<Pattern> copyOfParentPattern;
 
 		public SubPattern(string name, Parameters parameters, Pattern parentPattern) :
 			base(name, new Lambda(new FunctionInvokable("", Parameters.Empty, "")), parameters)
 		{
 			this.parentPattern = parentPattern;
-			copyOfParentPattern = none<Pattern>();
 		}
 
-		public IObject Value { get; set; } = Boolean.False;
+		public override string Image => $"pattern {parentPattern.Name}[{name}]";
+
+		public override bool IsEqualTo(IObject obj)
+		{
+			return obj is SubPattern subPattern && name == subPattern.name && parentPattern.IsEqualTo(subPattern.parentPattern);
+		}
+
+		//public override Pattern Copy() => new SubPattern(name, parameters, parentPattern.Copy());
+
+		public override void RegisterArguments(Arguments arguments) => parentPattern.RegisterArguments(arguments);
 
 		public override bool Match(IObject comparisand, Hash<string, IObject> bindings)
 		{
-			return returnValues(Value, bindings);
+			if (parentPattern.Match(comparisand, bindings))
+			{
+				if (IsEqualTo(parentPattern.Value))
+				{
+					return returnValues()
+				}
+			}
+			else
+			{
+				Value = Boolean.False;
+				return false;
+			}
 		}
 	}
 }
