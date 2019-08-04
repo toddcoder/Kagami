280a28747d87fc59d175fca858906138bd8d5ef4
Todd bennett <toddcoder@hotmail.com>
2019-08-03 20:04:12 -0500

    fixed MatchFunction

--- Kagami.Library/Nodes/Statements/MatchFunction.cs
+++ Kagami.Library/Nodes/Statements/MatchFunction.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Kagami.Library.Classes;
+﻿using Kagami.Library.Classes;
 using Kagami.Library.Invokables;
 using Kagami.Library.Objects;
 using Kagami.Library.Operations;
@@ -10,68 +9,70 @@ using static Kagami.Library.AllExceptions;
 
 namespace Kagami.Library.Nodes.Statements
 {
-   public class MatchFunction : Statement
-   {
-      Selector selector;
-      IObject[] comparisands;
-      Block block;
-      bool overriding;
-      string className;
+    public class MatchFunction : Statement
+    {
+        Selector selector;
+        Parameters parameters;
+        Block block;
+        bool overriding;
+        string className;
 
-      public MatchFunction(string functionName, IObject[] comparisands, Block block, bool overriding, string className)
-      {
-	      selector = new Selector(functionName, new SelectorItem[0], functionName);
-	      this.comparisands = comparisands;
-         this.overriding = overriding;
-         this.className = className;
-      }
+        public MatchFunction(string functionName, Parameters parameters, If ifStatement, bool overriding, string className)
+        {
+            selector = parameters.Selector(functionName);
+            this.parameters = parameters;
+            block = new Block(ifStatement) { new ReturnNothing() };
+            block.AddReturnIf();
+            this.overriding = overriding;
+            this.className = className;
+        }
 
-      public IInvokable getInvokable() => new FunctionInvokable(selector, new Parameters(comparisands.Length), ToString());
+        public IInvokable getInvokable() => new FunctionInvokable(selector, parameters, ToString());
 
-      public override void Generate(OperationsBuilder builder)
-      {
-         var invokable = getInvokable();
-         var lambda = new Lambda(invokable);
-         if (builder.RegisterInvokable(invokable, block, overriding).If(out _, out var exception))
-         {
-            if (!overriding)
+        public override void Generate(OperationsBuilder builder)
+        {
+            var invokable = getInvokable();
+            var lambda = new Lambda(invokable);
+            if (builder.RegisterInvokable(invokable, block, overriding).If(out _, out var exception))
             {
-	            builder.NewSelector(selector, false, true);
+                if (!overriding)
+                {
+                    builder.NewSelector(selector, false, true);
+                }
+
+                builder.PushObject(lambda);
+                builder.Peek(Index);
+                builder.AssignField(selector, overriding);
+            }
+            else
+            {
+                throw exception;
             }
 
-            builder.PushObject(lambda);
-            builder.Peek(Index);
-            builder.AssignField(selector, overriding);
-         }
-         else
-         {
-	         throw exception;
-         }
+            if (className.IsNotEmpty())
+            {
+                if (Module.Global.Class(className).If(out var cls))
+                {
+                    cls.RegisterMessage(selector, (obj, msg) => BaseClass.Invoke(obj, msg.Arguments, lambda));
+                }
+                else
+                {
+                    throw classNotFound(className);
+                }
+            }
+        }
 
-         if (className.IsNotEmpty())
-         {
-	         if (Module.Global.Class(className).If(out var cls))
-	         {
-		         cls.RegisterMessage(selector, (obj, msg) => BaseClass.Invoke(obj, msg.Arguments, lambda));
-	         }
-	         else
-	         {
-		         throw classNotFound(className);
-	         }
-         }
-      }
+        public override string ToString() => $"{overriding.Extend("override ")}match {selector.Image}() ...";
 
-      public override string ToString() => $"{overriding.Extend("override ")}match {selector.Image}() ...";
-
-      public void Deconstruct(out Selector selector, out Parameters parameters, out Block block, out bool yielding,
-         out IInvokable invokable, out bool overriding)
-      {
-         selector = this.selector;
-         parameters = this.parameters;
-         block = this.block;
-         yielding = false;
-         invokable = getInvokable();
-         overriding = this.overriding;
-      }
-   }
+        public void Deconstruct(out Selector selector, out Parameters parameters, out Block block, out bool yielding,
+           out IInvokable invokable, out bool overriding)
+        {
+            selector = this.selector;
+            parameters = this.parameters;
+            block = this.block;
+            yielding = false;
+            invokable = getInvokable();
+            overriding = this.overriding;
+        }
+    }
 }
