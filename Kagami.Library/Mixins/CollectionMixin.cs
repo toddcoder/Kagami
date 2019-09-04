9b9e3d634343ea56673272512ab6efe4aa2bf2fc
Todd bennett <toddcoder@hotmail.com>
2019-09-03 20:06:01 -0500

    prophylactic

---
+++ Kagami.Library/Mixins/CollectionMixin.cs
@@ -0,0 +1,30 @@
+﻿using Core.Monads;
+using Kagami.Library.Objects;
+
+namespace Kagami.Library.Mixins
+{
+	public class CollectionMixin : Mixin, ICollection
+	{
+		public CollectionMixin(string name) : base(name) { }
+
+		public IIterator GetIterator(bool lazy) => TODO_IMPLEMENT_ME;
+
+		public IMaybe<IObject> Next(int index) => TODO_IMPLEMENT_ME;
+
+		public IMaybe<IObject> Peek(int index) => TODO_IMPLEMENT_ME;
+
+		public Int Length { get; }
+
+		public bool ExpandForArray { get; }
+
+		public Boolean In(IObject item) => TODO_IMPLEMENT_ME;
+
+		public Boolean NotIn(IObject item) => TODO_IMPLEMENT_ME;
+
+		public IObject Times(int count) => TODO_IMPLEMENT_ME;
+
+		public String MakeString(string connector) => TODO_IMPLEMENT_ME;
+
+		public IIterator GetIndexedIterator() => TODO_IMPLEMENT_ME;
+	}
+}
