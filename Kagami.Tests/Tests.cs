6812d5948ffba731e38151fca55ff12a8deadb44
Todd Bennett <toddcoder@comcast.net>
2025-06-16 09:37:16 -0500

    Checkpoint

--- Kagami.Tests/Tests.cs
+++ Kagami.Tests/Tests.cs
@@ -1,12 +1,7 @@
-﻿using System.Reflection;
-using Core.Computers;
-
-namespace Kagami.Tests;
+﻿namespace Kagami.Tests;
 
 public class Tests
 {
-   protected Lazy<FolderName> testFolder = new(() => ((FileName)Assembly.GetExecutingAssembly().Location).Folder);
-
    protected void runTest()
    {
       
