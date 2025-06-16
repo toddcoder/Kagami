1e3c3921999c193caa138c763fc392a41f60263d
Todd Bennett <toddcoder@comcast.net>
2025-06-16 09:09:14 -0500

    Simplified concatenation

---
+++ Kagami.Library/Nodes/Symbols/ConcatenateSymbol.cs
@@ -0,0 +1,14 @@
+﻿using Kagami.Library.Operations;
+
+namespace Kagami.Library.Nodes.Symbols;
+
+public class ConcatenateSymbol : Symbol
+{
+   public override void Generate(OperationsBuilder builder) => builder.Concatenate();
+
+   public override Precedence Precedence => Precedence.Concatenate;
+
+   public override Arity Arity => Arity.Binary;
+
+   public override string ToString() => "~";
+}
--- Kagami.Library/Operations/Concatenate.cs
+++ Kagami.Library/Operations/Concatenate.cs
@@ -1,12 +1,41 @@
 ﻿using Core.Monads;
 using Kagami.Library.Objects;
 using Kagami.Library.Runtime;
+using static Kagami.Library.AllExceptions;
 
 namespace Kagami.Library.Operations;
 
 public class Concatenate : TwoOperandOperation
 {
-   public override Optional<IObject> Execute(Machine machine, IObject x, IObject y) => (KString)(x.AsString + y.AsString);
+   public override Optional<IObject> Execute(Machine machine, IObject x, IObject y)
+   {
+      switch (x, y)
+      {
+         case (KString xs, KString ys):
+            return (KString)(xs.Value + ys.Value);
+         case (KArray xa, KArray xb):
+            return xa.Concatenate(xb).Just();
+         default:
+         {
+            if (x is KString || y is KString)
+            {
+               return (KString)(x.AsString + y.AsString);
+            }
+            else
+            {
+               var _class = Module.Global.Value.Class(x.ClassName);
+               if (_class is (true, var @class))
+               {
+                  return @class.SendMessage(x, "~(_)", new Arguments(y)).Just();
+               }
+               else
+               {
+                  return classNotFound(x.ClassName);
+               }
+            }
+         }
+      }
+   }
 
    public override string ToString() => "concatenate";
 }
