7ad3862c4f6c4c0359fdf49540ab4f064dbc0a7d
Todd Bennett <toddcoder@comcast.net>
2025-06-16 09:28:32 -0500

    Added new concatenation operation to interpolated strings

--- Kagami.Library/Nodes/Symbols/InterpolatedStringSymbol.cs
+++ Kagami.Library/Nodes/Symbols/InterpolatedStringSymbol.cs
@@ -1,53 +1,50 @@
-﻿using System;
-using System.Linq;
-using Kagami.Library.Operations;
+﻿using Kagami.Library.Operations;
 using Core.Enumerables;
 using Core.Strings;
 
-namespace Kagami.Library.Nodes.Symbols
+namespace Kagami.Library.Nodes.Symbols;
+
+public class InterpolatedStringSymbol : Symbol
 {
-   public class InterpolatedStringSymbol : Symbol
+   protected string prefix;
+   protected Expression[] expressions;
+   protected string[] suffixes;
+   protected bool isFailure;
+
+   public InterpolatedStringSymbol(string prefix, Expression[] expressions, string[] suffixes, bool isFailure)
    {
-      protected string prefix;
-      protected Expression[] expressions;
-      protected string[] suffixes;
-      protected bool isFailure;
+      this.prefix = prefix;
+      this.expressions = expressions;
+      this.suffixes = suffixes;
+      this.isFailure = isFailure;
+   }
 
-      public InterpolatedStringSymbol(string prefix, Expression[] expressions, string[] suffixes, bool isFailure)
+   public override void Generate(OperationsBuilder builder)
+   {
+      builder.PushString(prefix);
+
+      var length = Math.Min(expressions.Length, suffixes.Length);
+      for (var i = 0; i < length; i++)
       {
-         this.prefix = prefix;
-         this.expressions = expressions;
-         this.suffixes = suffixes;
-         this.isFailure = isFailure;
+         expressions[i].Generate(builder);
+         builder.String();
+         builder.Concatenate();
+         builder.PushString(suffixes[i]);
+         builder.Concatenate();
       }
 
-      public override void Generate(OperationsBuilder builder)
+      if (isFailure)
       {
-         builder.PushString(prefix);
-
-         var length = Math.Min(expressions.Length, suffixes.Length);
-         for (var i = 0; i < length; i++)
-         {
-            expressions[i].Generate(builder);
-            builder.String();
-            builder.SendMessage("~(_)", 1);
-            builder.PushString(suffixes[i]);
-            builder.SendMessage("~(_)", 1);
-         }
-
-         if (isFailure)
-         {
-            builder.Failure();
-         }
+         builder.Failure();
       }
+   }
 
-      public override Precedence Precedence => Precedence.Value;
+   public override Precedence Precedence => Precedence.Value;
 
-      public override Arity Arity => Arity.Nullary;
+   public override Arity Arity => Arity.Nullary;
 
-      public override string ToString()
-      {
-         return (StringStream)"$\"" / prefix / expressions.Zip(suffixes, (e, s) => $"({e}){s}").ToString("");
-      }
+   public override string ToString()
+   {
+      return (StringStream)"$\"" / prefix / expressions.Zip(suffixes, (e, s) => $"({e}){s}").ToString("");
    }
 }
