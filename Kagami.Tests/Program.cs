0a9dc4945cd384a36250d62e19ef416a4eb30d33
Todd Bennett <toddcoder@comcast.net>
2025-06-16 08:04:47 -0500

    Added test project

---
+++ Kagami.Tests/Program.cs
@@ -0,0 +1,1 @@
+﻿var test = new Kagami.Tests.Tests();
---
+++ Kagami.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Core.Computers;
+
+namespace Kagami.Tests;
+
+public class Tests
+{
+   protected Lazy<FolderName> testFolder = new(() => ((FileName)Assembly.GetExecutingAssembly().Location).Folder);
+
+   protected void runTest()
+   {
+      
+   }
+}
