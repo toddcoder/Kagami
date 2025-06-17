4099fd2b7efd4476ed7809e3ff9e6c4451fad5f1
toddc <toddc@GANDALF>
2025-06-16 22:00:51 -0500

    loop until can be loop while also

--- Kagami.Library/Nodes/Statements/Loop.cs
+++ Kagami.Library/Nodes/Statements/Loop.cs
@@ -2,53 +2,61 @@
 using Kagami.Library.Operations;
 using static Kagami.Library.Nodes.NodeFunctions;
 
-namespace Kagami.Library.Nodes.Statements
+namespace Kagami.Library.Nodes.Statements;
+
+public class Loop : Statement
 {
-   public class Loop : Statement
+   protected Block block;
+   protected Expression expression;
+   protected bool isUntil;
+
+   public Loop(Block block, Expression expression, bool isUntil)
    {
-      protected Block block;
-      protected Expression expression;
+      this.block = block;
+      this.expression = expression;
+      this.isUntil = isUntil;
+   }
 
-      public Loop(Block block, Expression expression)
+   public override void Generate(OperationsBuilder builder)
+   {
+      var beginLabel = newLabel("begin");
+      var exitLabel = newLabel("exit");
+      var skipLabel = newLabel("skip");
+      var controlLabel = newLabel("control");
+      var endLabel = newLabel("end");
+
+      builder.Label(beginLabel);
+
+      builder.PushFrame();
+      builder.PushExitFrame(exitLabel);
+      builder.PushSkipFrame(skipLabel);
+
+      block.Generate(builder);
+
+      builder.PopFrame();
+      builder.Label(skipLabel);
+      builder.PopFrame();
+
+      expression.Generate(builder);
+      if (isUntil)
       {
-         this.block = block;
-         this.expression = expression;
+         builder.GoToIfFalse(controlLabel);
+      }
+      else
+      {
+         builder.GoToIfTrue(controlLabel);
       }
 
-      public override void Generate(OperationsBuilder builder)
-      {
-         var beginLabel = newLabel("begin");
-         var exitLabel = newLabel("exit");
-         var skipLabel = newLabel("skip");
-         var untilLabel = newLabel("until");
-         var endLabel = newLabel("end");
+      builder.PopFrame();
 
-         builder.Label(beginLabel);
+      builder.Label(exitLabel);
+      builder.GoTo(endLabel);
 
-         builder.PushFrame();
-         builder.PushExitFrame(exitLabel);
-         builder.PushSkipFrame(skipLabel);
+      builder.Label(controlLabel);
+      builder.PopFrame();
+      builder.GoTo(beginLabel);
 
-         block.Generate(builder);
-
-         builder.PopFrame();
-         builder.Label(skipLabel);
-         builder.PopFrame();
-
-         expression.Generate(builder);
-         builder.GoToIfFalse(untilLabel);
-
-         builder.PopFrame();
-
-         builder.Label(exitLabel);
-         builder.GoTo(endLabel);
-
-         builder.Label(untilLabel);
-         builder.PopFrame();
-         builder.GoTo(beginLabel);
-
-         builder.Label(endLabel);
-         builder.NoOp();
-      }
+      builder.Label(endLabel);
+      builder.NoOp();
    }
 }
