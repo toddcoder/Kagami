9f8ecbed4b3f85b9c51aec57162f636809a16df1
toddc <toddc@GANDALF>
2025-07-09 22:00:13 -0500

    Readded Out

---
+++ Kagami.Library/Packages/Out.cs
@@ -0,0 +1,37 @@
+﻿using Core.Collections;
+using Kagami.Library.Objects;
+using Kagami.Library.Runtime;
+using static Kagami.Library.Objects.ObjectFunctions;
+
+namespace Kagami.Library.Packages;
+
+public struct Out() : IObject
+{
+   public string ClassName => "Out";
+
+   public string AsString => "Out";
+
+   public string Image => "Out";
+
+   public int Hash => HashCode.Combine(ClassName);
+
+   public bool IsEqualTo(IObject obj) => obj is Out;
+
+   public bool Match(IObject comparisand, Hash<string, IObject> bindings) => match(this, comparisand, bindings);
+
+   public bool IsTrue => true;
+
+   public Guid Id { get; init; } = Guid.NewGuid();
+
+   public IObject Append(IObject item)
+   {
+      Machine.Current.Value.Context.Print(item.AsString);
+      return this;
+   }
+
+   public IObject AppendLine(IObject item)
+   {
+      Machine.Current.Value.Context.PrintLine(item.AsString);
+      return this;
+   }
+}
---
+++ Kagami.Library/Packages/OutClass.cs
@@ -0,0 +1,18 @@
+﻿using Kagami.Library.Classes;
+using Kagami.Library.Objects;
+using static Kagami.Library.Classes.ClassFunctions;
+
+namespace Kagami.Library.Packages;
+
+public class OutClass : BaseClass
+{
+   public override string Name => "Out";
+
+   public override void RegisterMessages()
+   {
+      base.RegisterMessages();
+
+      registerMessage("<<(_)", (obj, msg) => function<Out, IObject>(obj, msg, (o, a) => o.Append(a)));
+      registerMessage("<|(_)", (obj, msg) => function<Out, IObject>(obj, msg, (o, a) => o.AppendLine(a)));
+   }
+}
