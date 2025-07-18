b3b651efeef4a27f2bb36349cf257bc896f882cd
Todd Bennett <toddcoder@comcast.net>
2025-07-18 08:19:26 -0500

    Checkpoint

--- Kagami.Library/Operations/Operation.cs
+++ Kagami.Library/Operations/Operation.cs
@@ -1,12 +1,42 @@
-﻿using Kagami.Library.Objects;
+﻿using Core.Monads;
+using Kagami.Library.Objects;
 using Kagami.Library.Runtime;
-using Core.Monads;
+using static Kagami.Library.AllExceptions;
+using Machine = Kagami.Library.Runtime.Machine;
 
 namespace Kagami.Library.Operations;
 
 public abstract class Operation
 {
    public abstract Optional<IObject> Execute(Machine machine);
+
+   protected Optional<(IObject x, IObject y)> implicitConversion(Machine machine, IObject x, IObject y)
+   {
+      var fromClass = x.ClassName;
+      var toClass = y.ClassName;
+
+      var _selector = Module.Global.Value.GetConversion(fromClass, toClass);
+      if (_selector is (true, var selector))
+      {
+         var _field = machine.Find(selector);
+         if (_field is (true, var selectedField))
+         {
+            return Invoke.InvokeObject(machine, selectedField.Value, arguments, ref increment);
+         }
+         else if (_field.Exception is (true, var exception))
+         {
+            return exception;
+         }
+         else
+         {
+            return fieldNotFound(image);
+         }
+      }
+      else
+      {
+         return (x, y);
+      }
+   }
 
    public virtual bool Increment => true;
 }
