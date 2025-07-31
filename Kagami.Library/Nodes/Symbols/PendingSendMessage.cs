e827b51f4e28055c7c135e9baf967e40207bf41d
Todd Bennett <toddcoder@comcast.net>
2025-07-31 15:50:37 -0500

    Added pending send message

---
+++ Kagami.Library/Nodes/Symbols/PendingSendMessage.cs
@@ -0,0 +1,28 @@
+﻿using Kagami.Library.Nodes.Statements;
+using Kagami.Library.Operations;
+using Kagami.Library.Parsers.Expressions;
+using static Core.Monads.MonadFunctions;
+using Return = Kagami.Library.Nodes.Statements.Return;
+
+namespace Kagami.Library.Nodes.Symbols;
+
+public class PendingSendMessage(SendMessageSymbol sendMessageSymbol) : Symbol
+{
+   public override void Generate(OperationsBuilder builder)
+   {
+      var innerBuilder = new ExpressionBuilder(ExpressionFlags.Standard);
+      innerBuilder.Add(new FieldSymbol("__$0"));
+      innerBuilder.Add(sendMessageSymbol);
+      var expression = innerBuilder.ToExpression().ForceValue();
+      var returnStatement = new Return(expression, nil);
+      var block = new Block(returnStatement);
+      var lambda = new LambdaSymbol(1, block);
+      lambda.Generate(builder);
+   }
+
+   public override Precedence Precedence => Precedence.Value;
+
+   public override Arity Arity => Arity.Nullary;
+
+   public override string ToString() => $"_{sendMessageSymbol}";
+}
---
+++ Kagami.Library/Parsers/Expressions/PendingSendMessageParser.cs
@@ -0,0 +1,73 @@
+﻿using Core.Monads;
+using Core.Monads.Lazy;
+using Core.Strings;
+using Kagami.Library.Nodes.Symbols;
+using Kagami.Library.Objects;
+using System.Text.RegularExpressions;
+using static Core.Monads.MonadFunctions;
+using static Kagami.Library.Parsers.ParserFunctions;
+using Regex = System.Text.RegularExpressions.Regex;
+
+namespace Kagami.Library.Parsers.Expressions;
+
+public partial class PendingSendMessageParser : SymbolParser
+{
+   public PendingSendMessageParser(ExpressionBuilder builder) : base(builder)
+   {
+   }
+
+   [GeneratedRegex($@"^(\s*)(_)(\.)({REGEX_FUNCTION_NAME})(\()?")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> Parse(ParseState state, Token[] tokens, ExpressionBuilder builder)
+   {
+      var name = tokens[4].Text;
+      var parameterDelimiter = tokens[5].Text;
+      var parseArguments = true;
+
+      if (parameterDelimiter.IsEmpty())
+      {
+         name = name.get();
+         parseArguments = false;
+      }
+      else if (name.EndsWith("="))
+      {
+         if (builder.Flags[ExpressionFlags.OmitAssign])
+         {
+            return nil;
+         }
+
+         name = name.Drop(-1).set();
+         parseArguments = true;
+      }
+
+      if (parseArguments)
+      {
+         state.Colorize(tokens, Color.Whitespace, Color.Message, Color.Message, Color.Message, Color.OpenParenthesis);
+      }
+      else
+      {
+         state.Colorize(tokens, Color.Whitespace, Color.Message, Color.Message, Color.Message);
+      }
+
+      LazyOptional<(Expression[], Maybe<LambdaSymbol>)> _argumentsPlusLambda = nil;
+      if (!parseArguments)
+      {
+         Selector selector = name;
+         var sendMessageSymbol = new SendMessageSymbol(selector, Precedence.SendMessage, false);
+         builder.Add(new PendingSendMessage(sendMessageSymbol));
+      }
+      else if (_argumentsPlusLambda.ValueOf(getArgumentsPlusLambda(state, builder.Flags)) is (true, var (arguments, _lambda)))
+      {
+         var selector = name.Selector(arguments.Length);
+         var sendMessageSymbol = new SendMessageSymbol(selector, Precedence.SendMessage, false, _lambda, arguments);
+         builder.Add(new PendingSendMessage(sendMessageSymbol));
+      }
+      else
+      {
+         return _argumentsPlusLambda.Exception;
+      }
+
+      return unit;
+   }
+}
