965e6008203eaf357f78e3769b80fa35a005d70e
Todd Bennett <tebennett@eprod.com>
2025-10-03 09:55:40 -0500

    using / on Int results in Rational

--- Kagami.Library/Operations/FloatDivide.cs
+++ Kagami.Library/Operations/FloatDivide.cs
@@ -1,4 +1,5 @@
-﻿using Kagami.Library.Objects;
+﻿using System.Numerics;
+using Kagami.Library.Objects;
 using Kagami.Library.Runtime;
 using Core.Monads;
 using static Kagami.Library.Objects.ObjectFunctions;
@@ -9,20 +10,23 @@ public class FloatDivide : TwoOperandOperation
 {
    public override Optional<IObject> Execute(Machine machine, IObject x, IObject y)
    {
-      if (x is INumeric n1 && y is INumeric n2 && n1.IsPrimitive && n2.IsPrimitive)
+      switch (x)
       {
-         var dx = n1.AsDouble();
-         var dy = n2.AsDouble();
+         case Int i1 when y is Int i2:
+            return new Rational(new BigInteger(i1.Value), new BigInteger(i2.Value));
+         case Long l1 when y is Long l2:
+            return new Rational(l1.AsBigInteger(), l2.AsBigInteger());
+         case INumeric n1 when y is INumeric n2 && n1.IsPrimitive && n2.IsPrimitive:
+         {
+            var dx = n1.AsDouble();
+            var dy = n2.AsDouble();
 
-         return Float.FloatObject(dx / dy).Just();
-      }
-      else if (x is KIndex index && y is Int i)
-      {
-         return index.Contract(i.Value);
-      }
-      else
-      {
-         return sendMessage(x, "/(_)", y).Just();
+            return Float.FloatObject(dx / dy).Just();
+         }
+         case KIndex index when y is Int i:
+            return index.Contract(i.Value);
+         default:
+            return sendMessage(x, "/(_)", y).Just();
       }
    }
 
