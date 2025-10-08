0909d22cbf59a3ab5a8a7cc14f90d995198c184a
Todd Bennett <tebennett@eprod.com>
2025-10-08 10:01:07 -0500

    Checkpoint

---
+++ Kagami.Library/Nodes/Statements/Decorators.cs
@@ -0,0 +1,10 @@
+﻿using Kagami.Library.Operations;
+
+namespace Kagami.Library.Nodes.Statements;
+
+public class Decorators : Statement
+{
+   public override void Generate(OperationsBuilder builder)
+   {
+   }
+}
---
+++ Kagami.Library/Parsers/Statements/DecoratorParser.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using Core.Monads;
+using Kagami.Library.Nodes.Symbols;
+using Kagami.Library.Parsers.Expressions;
+using static Core.Monads.MonadFunctions;
+using static Kagami.Library.Parsers.ParserFunctions;
+
+namespace Kagami.Library.Parsers.Statements;
+
+public partial class DecoratorParser : StatementParser
+{
+   protected const string REGEX_INVOKE = @$"^(\s*)({REGEX_FUNCTION_NAME})(\()";
+
+   [GeneratedRegex(@"^(\s*)(\[)")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> ParseStatement(ParseState state, Token[] tokens)
+   {
+      state.Colorize(tokens, Color.Whitespace, Color.Structure);
+
+      List<InvokeSymbol> invokeSymbols = [];
+
+      while (state.More)
+      {
+         if (state.Scan(@"^(\s*)(\]"))
+         {
+            break;
+         }
+
+         var _result = state.Scan(REGEX_INVOKE, 2, Color.Whitespace, Color.Invokable);
+         if (_result is (true, var invokableName))
+         {
+            var _arguments = getArguments(state, ExpressionFlags.InArgument);
+            if (_arguments is (true, var arguments))
+            {
+               var invokeSymbol = new InvokeSymbol(invokableName, arguments, nil, false);
+               invokeSymbols.Add(invokeSymbol);
+            }
+         }
+      }
+
+      return unit;
+   }
+}
