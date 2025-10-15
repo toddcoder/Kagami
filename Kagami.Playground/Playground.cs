8c123d0943351a3ed68a54646a88f1358a6c427b
Todd Bennett <tebennett@eprod.com>
2025-10-15 13:41:34 -0500

    Modified Playground to use new print statements

--- Kagami.Playground/Playground.cs
+++ Kagami.Playground/Playground.cs
@@ -167,10 +167,9 @@ public partial class Playground : Form
          menus.Menu("&Insert");
          menus.Menu("open sys", (_, _) => insertText("open sys\n\n", 0, 0), "^%S");
          menus.Menu("open math", (_, _) => insertText("open math\n\n", 0, 0), "^%M");
-         menus.Menu("println() surround", (_, _) => surround("println(", ")"), "^%P");
-         menus.Menu("println() interpolated", (_, _) => insertText("println()", -1), "^P");
-         menus.Menu("print()", (_, _) => surround("print(", ")"));
-         menus.Menu("put()", (_, _) => surround("put(", ")"));
+         menus.Menu("println", (_, _) => insertText("println ", 0, 0), "^P");
+         menus.Menu("print", (_, _) => insertText("print ", 0, 0));
+         menus.Menu("put", (_, _) => insertText("put ", 0, 0));
          menus.Menu("peek()", (_, _) => surround("peek(", ")"), "^K");
          menus.Menu("Triple quotes", (_, _) => insertText("\"\"\"\n\"\"\"", -3), "^Q");
 
