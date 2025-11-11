b94b50e565b525b4dba05cd8fdb47f286b547014
Todd Bennett <toddcoder@comcast.net>
2025-11-11 12:59:24 -0600

    Added callable expression

---
+++ Kagami.Library/Parsers/Expressions/CallableExpressionParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using Core.Monads;
+using Kagami.Library.Nodes.Symbols;
+using static Core.Monads.MonadFunctions;
+using static Kagami.Library.Parsers.ParserFunctions;
+
+namespace Kagami.Library.Parsers.Expressions;
+
+public partial class CallableExpressionParser : SymbolParser
+{
+   public CallableExpressionParser(ExpressionBuilder builder) : base(builder)
+   {
+   }
+
+   [GeneratedRegex(@"^(\s*)(\^)")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> Parse(ParseState state, Token[] tokens, ExpressionBuilder builder)
+   {
+      state.Colorize(tokens, Color.Whitespace, Color.Lambda);
+
+      var _expression = getExpression(state, ExpressionFlags.InLambda);
+      if (_expression is (true, var expression))
+      {
+         builder.Add(new LambdaSymbol(0, expression));
+         return unit;
+      }
+      else
+      {
+         return _expression.Exception;
+      }
+   }
+}
