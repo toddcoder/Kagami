bc385679c579965513cf59f037c81bb4a86cd689
Todd Bennett <toddcoder@comcast.net>
2026-01-27 13:39:59 -0600

    Checkpoint

---
+++ Kagami.Library/Parsers/Statements/AnnotationParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using Core.Monads;
+using static Core.Monads.MonadFunctions;
+
+namespace Kagami.Library.Parsers.Statements;
+
+public partial class AnnotationParser : StatementParser
+{
+   [GeneratedRegex(@"^@\w+")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> ParseStatement(ParseState state, Token[] tokens) => nil;
+}
