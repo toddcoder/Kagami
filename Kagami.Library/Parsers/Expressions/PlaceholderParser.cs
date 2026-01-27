c4c3572c4fe670d04b24d22b86ab6347f7e41ff4
Todd Bennett <toddcoder@comcast.net>
2026-01-27 09:56:25 -0600

    Fixed user|var bug for placeholders

--- Kagami.Library/Parsers/Expressions/PlaceholderParser.cs
+++ Kagami.Library/Parsers/Expressions/PlaceholderParser.cs
@@ -13,7 +13,7 @@ public partial class PlaceholderParser : SymbolParser
    {
    }
 
-   [GeneratedRegex($@"^(\s*)((?:use|var)\s*)?({REGEX_FIELD})\b(?!"")")]
+   [GeneratedRegex($@"^(\s*)((?:use|var)\s+)?({REGEX_FIELD})\b(?!"")")]
    public override partial Regex Regex();
 
    public override Optional<Unit> Parse(ParseState state, Token[] tokens, ExpressionBuilder builder)
