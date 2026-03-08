7d852f10c2f8dcad5000e4b4448c2a3ef924a261
toddc <toddc@GANDALF>
2026-03-07 21:02:42 -0600

    Finalize

---
+++ Kagami.Library/Nodes/Symbols/DecimalRationalSymbol.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using Kagami.Library.Objects;
+using Kagami.Library.Operations;
+
+namespace Kagami.Library.Nodes.Symbols;
+
+public class DecimalRationalSymbol(BigInteger numerator, BigInteger denominator) : Symbol, IConstant
+{
+   protected Rational rational = new(numerator, denominator);
+
+   public override void Generate(OperationsBuilder builder) => builder.PushObject(rational);
+
+   public override Precedence Precedence => Precedence.Value;
+
+   public override Arity Arity => Arity.Nullary;
+
+   public override string ToString() => $"{numerator}|{denominator}";
+
+   public IObject Object => rational;
+}
--- Kagami.Library/Parsers/Expressions/FloatParser.cs
+++ Kagami.Library/Parsers/Expressions/FloatParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
 using Kagami.Library.Nodes.Symbols;
 using Core.Monads;
 using static Kagami.Library.AllExceptions;
@@ -12,7 +13,7 @@ public partial class FloatParser : SymbolParser
    {
    }
 
-   [GeneratedRegex(@"^(\s*)(\d[\d_`]*\.\d[\d_`]*)(?:([eE])([-\+]?\d+))?(i|d|f)?")]
+   [GeneratedRegex(@"^(\s*)(\d[\d_`]*\.\d[\d_`]*)(?:([eE])([-\+]?\d+))?(i|d|f|r)?")]
    public override partial Regex Regex();
 
    public override Optional<Unit> Parse(ParseState state, Token[] tokens, ExpressionBuilder builder)
@@ -21,34 +22,142 @@ public partial class FloatParser : SymbolParser
       var type = tokens[5].Text;
       state.Colorize(tokens, Color.Whitespace, Color.Number, Color.NumberPart, Color.Number, Color.NumberPart);
 
-      if (type == "d")
+      switch (type)
       {
-         if (decimal.TryParse(source, out var decimalResult))
+         case "d" when decimal.TryParse(source, out var decimalResult):
          {
             builder.Add(new DecimalSymbol(decimalResult));
             return unit;
          }
-         else
-         {
+         case "d":
             return unableToConvert(source, "Decimal");
+         case "r":
+         {
+            var _decimalRational = getDecimalRational(source);
+            if (_decimalRational is (true, var (numerator, denominator)))
+            {
+               builder.Add(new DecimalRationalSymbol(numerator, denominator));
+               return unit;
+            }
+            else
+            {
+               return _decimalRational.Exception;
+            }
+         }
+         default:
+         {
+            if (double.TryParse(source, out var result))
+            {
+               if (type == "i")
+               {
+                  builder.Add(new ComplexSymbol(result));
+               }
+               else
+               {
+                  builder.Add(new FloatSymbol(result));
+               }
+
+               return unit;
+            }
+            else
+            {
+               return unableToConvert(source, "Float");
+            }
          }
       }
-      else if (double.TryParse(source, out var result))
+
+      static Optional<(BigInteger numerator, BigInteger denominator)> getDecimalRational(string source)
       {
-         if (type == "i")
+         var s = source;
+         var exp = 0;
+
+         var eIndex = s.IndexOfAny(['e', 'E']);
+         if (eIndex >= 0)
          {
-            builder.Add(new ComplexSymbol(result));
-         }
-         else
-         {
-            builder.Add(new FloatSymbol(result));
+            var expPart = s[(eIndex + 1)..];
+            s = s[..eIndex];
+
+            if (!int.TryParse(expPart, out exp))
+            {
+               return nil;
+            }
          }
 
-         return unit;
+         var sign = 1;
+         if (s.StartsWith('-'))
+         {
+            sign = -1;
+            s = s[1..];
+         }
+         else if (s.StartsWith('+'))
+         {
+            s = s[1..];
+         }
+
+         var dotIndex = s.IndexOf('.');
+         if (dotIndex < 0)
+         {
+            if (!BigInteger.TryParse(s, out var intVal))
+            {
+               return nil;
+            }
+
+            var numerator = intVal * sign;
+            var denominator = BigInteger.One;
+
+            switch (exp)
+            {
+               case > 0:
+                  numerator *= BigInteger.Pow(10, exp);
+                  break;
+               case < 0:
+                  denominator *= BigInteger.Pow(10, -exp);
+                  break;
+            }
+
+            var gcd = gcdAbs(numerator, denominator);
+            return (numerator / gcd, denominator / gcd);
+         }
+
+         var intPart = s[..dotIndex];
+         var fracPart = s[(dotIndex + 1)..];
+
+         var digits = intPart + fracPart;
+         if (digits.Length == 0 || !BigInteger.TryParse(digits, out var rawNumerator))
+         {
+            return nil;
+         }
+
+         var numeratorResult = rawNumerator * sign;
+         var denominatorResult = BigInteger.Pow(10, fracPart.Length);
+
+         switch (exp)
+         {
+            case > 0:
+               numeratorResult *= BigInteger.Pow(10, exp);
+               break;
+            case < 0:
+               denominatorResult *= BigInteger.Pow(10, -exp);
+               break;
+         }
+
+         var g = gcdAbs(numeratorResult, denominatorResult);
+         return (numeratorResult / g, denominatorResult / g);
       }
-      else
+
+      static BigInteger gcdAbs(BigInteger a, BigInteger b)
       {
-         return unableToConvert(source, "Float");
+         a = BigInteger.Abs(a);
+         b = BigInteger.Abs(b);
+
+         while (b != BigInteger.Zero)
+         {
+            var t = a % b;
+            a = b;
+            b = t;
+         }
+
+         return a == BigInteger.Zero ? BigInteger.One : a;
       }
    }
 }
