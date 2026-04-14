ccd5279b960b677cd884a439edba6aa8856818af
Todd Bennett <toddcoder@comcast.net>
2026-04-14 15:31:52 -0500

    Minor

--- Kagami.Library/Nodes/Statements/Pass.cs
+++ Kagami.Library/Nodes/Statements/Pass.cs
@@ -4,7 +4,9 @@ namespace Kagami.Library.Nodes.Statements;
 
 public class Pass : Statement
 {
-   public override void Generate(OperationsBuilder builder) => builder.NoOp();
+   public override void Generate(OperationsBuilder builder)
+   {
+   }
 
    public override string ToString() => "pass";
 }
