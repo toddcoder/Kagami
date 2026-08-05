9ff2ab72073b5e9beaf403ed6b1f550b305c6825
Todd Bennett <toddcoder@comcast.net>
2026-08-05 15:15:45 -0500

    format i calls __()

--- Kagami.Library/Nodes/Symbols/FormatSymbol.cs
+++ Kagami.Library/Nodes/Symbols/FormatSymbol.cs
@@ -1,10 +1,27 @@
 ﻿using Kagami.Library.Operations;
+using static Kagami.Library.Nodes.NodeFunctions;
 
 namespace Kagami.Library.Nodes.Symbols;
 
 public class FormatSymbol : Symbol
 {
-   public override void Generate(OperationsBuilder builder) => builder.SendMessage("format(_)", 1);
+   public override void Generate(OperationsBuilder builder)
+   {
+      var imageLabel = newLabel("image");
+      var endLabel = newLabel("end");
+
+      builder.Dup();
+      builder.PushString("i");
+      builder.Equal();
+      builder.GoToIfTrue(imageLabel);
+      builder.SendMessage("format(_)", 1);
+      builder.GoTo(endLabel);
+      builder.Label(imageLabel);
+      builder.Drop();
+      builder.SendMessage("image".get(), 0);
+      builder.Label(endLabel);
+      builder.NoOp();
+   }
 
    public override Precedence Precedence => Precedence.Format;
 
