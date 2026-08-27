c8e398afceb0ba1d873dffbf9688052799f16b35
Todd Bennett <toddcoder@comcast.net>
2026-08-27 10:01:22 -0500

    Checkpoint

---
+++ Kagami.Library/Nodes/Statements/BuilderYield.cs
@@ -0,0 +1,15 @@
+﻿using Kagami.Library.Nodes.Symbols;
+using Kagami.Library.Operations;
+using Kagami.Library.Parsers.Statements;
+
+namespace Kagami.Library.Nodes.Statements;
+
+public class BuilderYield(BuilderState builderState, Expression expression) : BuilderStatement(builderState), IHasExpression
+{
+   public override void Generate(OperationsBuilder builder)
+   {
+      Prefix(builder);
+   }
+
+   public Expression Expression => expression;
+}
