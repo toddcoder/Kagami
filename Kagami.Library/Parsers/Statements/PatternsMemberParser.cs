792c0fbabbc884141262436709c98980286de166
Todd Bennett <toddcoder@comcast.net>
2026-08-31 12:27:00 -0500

    Added patterns statement

---
+++ Kagami.Library/Parsers/Statements/PatternsMemberParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using Core.Monads;
+using Kagami.Library.Invokables;
+using Kagami.Library.Nodes.Statements;
+using static Core.Monads.MonadFunctions;
+using static Kagami.Library.Parsers.ParserFunctions;
+
+namespace Kagami.Library.Parsers.Statements;
+
+public partial class PatternsMemberParser(Parameters parameters) : StatementParser
+{
+   [GeneratedRegex(@$"^(\s*)(\|)(\s*)({REGEX_CLASS})\b")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> ParseStatement(ParseState state, Token[] tokens)
+   {
+      var patternName = tokens[4].Text;
+      state.Colorize(tokens, Color.Whitespace, Color.Operator, Color.Whitespace, Color.Class);
+      state.CreateReturnType();
+
+      var _block = getAnyBlock(state);
+      if (_block is (true, var block))
+      {
+         state.RemoveReturnType();
+         state.RegisterPattern(patternName);
+         state.AddStatement(new PatternStatement(patternName, parameters, block));
+
+         return unit;
+      }
+      else
+      {
+         state.RemoveReturnType();
+         return _block.Exception;
+      }
+   }
+}
---
+++ Kagami.Library/Parsers/Statements/PatternsParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+using Core.Monads;
+using static Core.Monads.MonadFunctions;
+using static Kagami.Library.Parsers.ParserFunctions;
+
+namespace Kagami.Library.Parsers.Statements;
+
+public partial class PatternsParser : StatementParser
+{
+   [GeneratedRegex($@"^(\s*)(patterns)(\()")]
+   public override partial Regex Regex();
+
+   public override Optional<Unit> ParseStatement(ParseState state, Token[] tokens)
+   {
+      state.Colorize(tokens, Color.Whitespace, Color.Keyword, Color.OpenParenthesis);
+
+      var _parameters = getParameters(state);
+      if (_parameters is (true, var parameters))
+      {
+         var _scan = state.BeginBlock();
+         if (_scan)
+         {
+            while (state.More)
+            {
+               _scan = state.EndBlock();
+               if (_scan)
+               {
+                  break;
+               }
+               else if (_scan.Exception is (true, var exception))
+               {
+                  return exception;
+               }
+
+               var patternsMemberParser = new PatternsMemberParser(parameters);
+               _scan = patternsMemberParser.Scan(state);
+               if (!_scan)
+               {
+                  return _scan.Exception;
+               }
+            }
+         }
+         else
+         {
+            return _scan.Exception;
+         }
+      }
+
+      return unit;
+   }
+}
